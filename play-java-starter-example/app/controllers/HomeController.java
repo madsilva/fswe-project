00898fc1c74d8a89246830edfd03885fd5189777
Nabeel Ahmad Khan <nabeelahmadkh@gmail.com>
2018-03-05 19:22:11 -0600

    added login functionality with sql

--- play-java-starter-example/app/controllers/HomeController.java
+++ play-java-starter-example/app/controllers/HomeController.java
@@ -1,9 +1,13 @@
 package controllers;
+
 import play.mvc.*;
 import views.html.*;
 import models.*;
 import javax.inject.Inject;
 import play.data.*;
+//
+//import anorm._;
+//import play.api.db.DB;
 
 import play.mvc.*;
 
@@ -29,10 +33,11 @@ public class HomeController extends Controller {
         return ok(index.render("Your new application is ready."));
     }
 
+
     public Result login(){
         Form<LoginData> loginForm = formFactory.form(LoginData.class);
         System.out.println("Login Function hit");
-        return ok(login.render(loginForm));
+        return ok(login.render(loginForm, " "));
     }
 
     public Result signup(){
@@ -40,7 +45,20 @@ public class HomeController extends Controller {
     }
 
     public Result profile(){
-        return ok(profile.render());
+
+        return ok(profile.render("temporary"));
+    }
+
+    public Result logout(){
+        session().clear();
+        System.out.println("Session cleared");
+        Form<LoginData> loginForm = formFactory.form(LoginData.class);
+        System.out.println("Login Function hit");
+        return ok(login.render(loginForm,"User Logged Out Successfully"));
+    }
+
+    public Result error(){
+        return ok(error.render());
     }
 
     public Result userlogin(){
@@ -50,23 +68,44 @@ public class HomeController extends Controller {
         LoginData loginCredentials = loginForm.get();
         System.out.println("UserDetails are");
 
-        boolean temp = loginCredentials.findLogin(loginCredentials.username, loginCredentials.password);
+        //LoginData login = LoginData.find.byId(loginCredentials.username);
+        //LoginData login = LoginData.find.where().eq("username", loginCredentials.username).eq("password", loginCredentials.password).findUnique();
+        LoginData login = LoginData.find.query().where().eq("username", loginCredentials.username).eq("password", loginCredentials.password).findUnique();
 
-        if (loginForm.hasErrors()) {
-            System.out.println("in");
-            return badRequest(login.render(loginForm));
-        } else {
-            if (temp) {
-                System.out.println("in else if");
-                session().clear();
-                // Creating a new session with the
-                session("email", loginForm.get().username);
-                return redirect(routes.HomeController.profile());
-            }
-            else{
-                System.out.println("in else else");
-                return badRequest(login.render(loginForm));
-            }
+        if (login == null){
+            System.out.println("User Not Found");
+            return ok(error.render());
+
         }
+        else{
+            System.out.println("User Logged In");
+            session("email", loginForm.get().username);
+            return ok(profile.render(loginForm.get().username));
+        }
+
+
+//        System.out.println("Result is");
+//        System.out.println(login);
+
+
+//        boolean temp = loginCredentials.findLogin(loginCredentials.username, loginCredentials.password);
+//
+//        if (loginForm.hasErrors()) {
+//            System.out.println("in");
+//            return badRequest(login.render(loginForm));
+//        } else {
+//            if (temp) {
+//                System.out.println("in else if");
+//                session().clear();
+//                // Creating a new session with the
+//                session("email", loginForm.get().username);
+//                return redirect(routes.HomeController.profile());
+//            }
+//            else{
+//                System.out.println("in else else");
+//                return badRequest(login.render(loginForm));
+//            }
+//        }
+
     }
 }
--- play-java-starter-example/app/models/LoginData.java
+++ play-java-starter-example/app/models/LoginData.java
@@ -1,44 +1,64 @@
 package models;
+//package javaguide.sql;
 
 import java.util.HashSet;
 import java.util.Set;
+import play.data.validation.Constraints;
 
-public class LoginData{
+//import javax.inject.Inject;
+//import javax.inject.Singleton;
+//
+//import play.mvc.Controller;
+//import play.db.NamedDatabase;
+//import play.db.Database;
+//import com.avaje.ebean.Model;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import io.ebean.*;
+
+
+@Entity
+public class LoginData extends Model{
+
+    @Id @Constraints.Required
     public String username;
+    @Constraints.Required
     public String password;
 
-    // Constructer for the class LoginData
-    // Default Constructor
-    public LoginData(){
+    public static Finder<String, LoginData> find = new Finder<>(LoginData.class);
 
-    }
-    // Custom Constructor
-    public LoginData(String username, String password){
-        this.username = username;
-        this.password = password;
-    }
-
-    private static Set<LoginData> logins;
-
-
-    static {
-        logins = new HashSet<LoginData>();
-        logins.add(new LoginData( "nabeelkhan", "123456"));
-        logins.add(new LoginData( "nabeelkh", "123456"));
-        logins.add(new LoginData("nabeel", "password"));
-    }
-
-    public boolean findLogin(String username, String password){
-        for (LoginData user : logins){
-            if (username.equals(user.username) && password.equals(user.password)){
-                return true;
-            }
-        }
-        // If the user is not present then return 0
-        return false;
-    }
-
-    public static void add(LoginData data){
-        logins.add(data);
-    }
+//    // Constructer for the class LoginData
+//    // Default Constructor
+//    public LoginData(){
+//
+//    }
+//    // Custom Constructor
+//    public LoginData(String username, String password){
+//        this.username = username;
+//        this.password = password;
+//    }
+//
+//    private static Set<LoginData> logins;
+//
+//
+//    static {
+//        logins = new HashSet<LoginData>();
+//        logins.add(new LoginData( "nabeelkhan", "123456"));
+//        logins.add(new LoginData( "nabeelkh", "123456"));
+//        logins.add(new LoginData("nabeel", "password"));
+//    }
+//
+//    public boolean findLogin(String username, String password){
+//        for (LoginData user : logins){
+//            if (username.equals(user.username) && password.equals(user.password)){
+//                return true;
+//            }
+//        }
+//        // If the user is not present then return 0
+//        return false;
+//    }
+//
+//    public static void add(LoginData data){
+//        logins.add(data);
+//    }
 }
